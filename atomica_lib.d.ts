4018ed982bece9d09ce020f8aafefafc668946e3
BobDaGithubAccount <BobDaGithubAccount@users.noreply.github.com>
2025-07-27 15:09:57 +0000

    deploy: a9d0de5f22669c1cca044f452ce0a5cec9861713

--- atomica_lib.d.ts
+++ atomica_lib.d.ts
@@ -18,10 +18,10 @@ export interface InitOutput {
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_6: WebAssembly.Table;
-  readonly closure4_externref_shim: (a: number, b: number, c: any) => void;
-  readonly closure387_externref_shim: (a: number, b: number, c: any) => void;
-  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h488a0355bbc8fdf8: (a: number, b: number) => void;
-  readonly closure413_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure5_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure397_externref_shim: (a: number, b: number, c: any) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h03406aa0fdb409d7: (a: number, b: number) => void;
+  readonly closure426_externref_shim: (a: number, b: number, c: any) => void;
   readonly __wbindgen_start: () => void;
 }
 
